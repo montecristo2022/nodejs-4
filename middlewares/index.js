c06bfb009a1a73b46ffbe57d148549f13ba903d5
montecristo2022 <104501641+montecristo2022@users.noreply.github.com>
2023-06-05 17:32:47 +0300

    Initial commit

---
+++ middlewares/index.js
@@ -0,0 +1,3 @@
+const validateBody = require("./validateBody");
+const authenticate = require("./authenticate")
+module.exports = { validateBody, authenticate };
---
+++ routes/api/auth.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const ctrl = require('../../controllers/auth')
+
+const router = express.Router();
+
+const { validateBody, authenticate } = require('../../middlewares');
+
+const { schemas } = require('../../models/user')
+
+// sign up
+
+router.post('/register', validateBody(schemas.registerSchema), ctrl.register);
+
+// sign in
+
+router.post('/login', validateBody(schemas.loginSchema), ctrl.login);
+
+router.get('/current', authenticate, ctrl.getCurrent)
+
+router.post('/logout', authenticate, ctrl.logout)
+
+
+module.exports = router
